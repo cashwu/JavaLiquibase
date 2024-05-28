8c6604760abe2fa24acd2369745dac0b7b1ec3d8
Cash <cashwugeek@icloud.com>
2024-05-28 15:18:54 +0800

    init

---
+++ src/main/java/com/cashwu/javaliquibase/JavaLiquibaseApplication.java
@@ -0,0 +1,13 @@
+package com.cashwu.javaliquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLiquibaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaLiquibaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cashwu/javaliquibase/JavaLiquibaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cashwu.javaliquibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLiquibaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
